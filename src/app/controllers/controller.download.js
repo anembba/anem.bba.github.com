b7c1bc6a1e946e4dc24e192916b8ce1222223676
malekbenz <malekbenz@gmail.com>
2017-01-05 19:35:35 +0100

    remove console.log

--- src/app/controllers/controller.download.js
+++ src/app/controllers/controller.download.js
@@ -12,21 +12,19 @@
         self.contents = "/dist";
         $rootScope.currentPath = $location.path();
 
-        var slideUrl = "https://anem.info/api/documents.json";
+        var url = "https://anem.info/api/documents.json";
         function getFileName(fullPath) {
             var filename = fullPath.replace(/^.*[\\\/]/, '');
             return filename.substring(0, filename.indexOf('.'))
 
         };
 
-        $http.get(slideUrl)
+        $http.get(url)
             .then(function (response) {
                 angular.forEach(response.data.docs, function (doc) {
                     doc.filename = getFileName(doc.path);
-
                 });
                 self.documents = response.data;
-                console.log(self.documents);
             });
 
     };
