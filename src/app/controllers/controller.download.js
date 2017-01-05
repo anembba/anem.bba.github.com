f7ea232b8c34b29d20f0ceed833855aa8f73ca57
malekbenz <malekbenz@gmail.com>
2017-01-05 19:12:45 +0100

    Add docs and download controller

--- src/app/controllers/controller.download.js
+++ src/app/controllers/controller.download.js
@@ -3,12 +3,31 @@
 
     app.controller("controller.download", controller);
 
-    controller.$inject = [ "$routeParams", "$location", "$rootScope" ];
+    controller.$inject = ["$routeParams", "$location", "$rootScope", "$http"];
 
-    function controller( $routeParams, $location, $rootScope ) {
+    function controller($routeParams, $location, $rootScope, $http) {
         var self = this;
-        self.contents  = "/dist";
+        self.title = "Documents à télécharger";
+        self.documents = {};
+        self.contents = "/dist";
         $rootScope.currentPath = $location.path();
 
+        var slideUrl = "https://anem.info/api/documents.json";
+        function getFileName(fullPath) {
+            var filename = fullPath.replace(/^.*[\\\/]/, '');
+            return filename.substring(0, filename.indexOf('.'))
+
+        };
+
+        $http.get(slideUrl)
+            .then(function (response) {
+                angular.forEach(response.data.docs, function (doc) {
+                    doc.filename = getFileName(doc.path);
+
+                });
+                self.documents = response.data;
+                console.log(self.documents);
+            });
+
     };
-}(angular.module("app")));        
+} (angular.module("app")));        
