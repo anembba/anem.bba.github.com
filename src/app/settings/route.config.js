b01cdabad6ad920db88112b61c4bdd47c557f60c
malekbenz <malekbenz@gmail.com>
2017-01-06 22:47:00 +0100

    remove old route

--- src/app/settings/route.config.js
+++ src/app/settings/route.config.js
@@ -3,12 +3,10 @@
 
     app.config(configure);
 
-    // configure.$inject = ["$routeProvider", "$locationProvider"];
     configure.$inject = ["$stateProvider", "$urlRouterProvider"];
 
-
     function configure($stateProvider, $urlRouterProvider) {
-        
+
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
@@ -36,42 +34,5 @@
                 controller: "controller.download",
                 controllerAs: "vm"
             });
-            };
- 
-
-        function configureRoute($routeProvider, $locationProvider) {
-            $routeProvider
-                .when("/about", {
-                    templateUrl: "views/about.html",
-                    controller: "controller.main as vm",
-                    controllerAs: "vm"
-
-                })
-                .when("/", {
-                    templateUrl: "views/main.html",
-                    controller: "controller.main as vm",
-                    controllerAs: "vm"
-
-                })
-                .when("/contact", {
-                    templateUrl: "views/contact.html",
-                    controller: "controller.contact as vm",
-                    controllerAs: "vm"
-
-                })
-                .when("/download", {
-                    templateUrl: "views/download.html",
-                    controller: "controller.download as vm",
-                    controllerAs: "vm"
-
-                })
-                .otherwise({ redirectTo: '/' });
-
-            $locationProvider.html5Mode(true);
-
-            // ng-storage config
-            //  $localStorageProvider.setKeyPrefix('bookstore');
-
-    }
-
-    } (angular.module("app")));
+    };
+} (angular.module("app")));
