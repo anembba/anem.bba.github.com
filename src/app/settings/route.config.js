2eec639ed50790ab0bcb8d9a7102e0403d7a3ac6
malekbenz <malekbenz@gmail.com>
2017-01-20 17:27:49 +0100

    add gems redirect

--- src/app/settings/route.config.js
+++ src/app/settings/route.config.js
@@ -8,7 +8,7 @@
     function configure($stateProvider, $urlRouterProvider, $locationProvider) {
 
         $urlRouterProvider.otherwise('/');
-        $locationProvider.html5Mode(true);
+        // $locationProvider.html5Mode(true);
         $stateProvider
             .state('home', {
                 url: '/',
